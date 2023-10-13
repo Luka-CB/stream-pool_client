50592f695ad543697d00a71f176de316ddaadcda
luka_CB <lukaaslamazashvili20@gmail.com>
2023-10-13 13:34:38 +0400

    Some Changes

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://stream-pool-api.onrender.com",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
